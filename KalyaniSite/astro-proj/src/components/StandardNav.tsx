ce7af7be262956a037e7f63f36e02a6d3948bffc
csdoge22 <sunnyambashta@gmail.com>
2024-09-23 21:16:14 -0400

    Changed the styling

--- KalyaniSite/astro-proj/src/components/StandardNav.tsx
+++ KalyaniSite/astro-proj/src/components/StandardNav.tsx
@@ -2,23 +2,23 @@ const StandardNav = () => {
     return (
         // navbar-expand ensures everything is displayed horizontally
         <>
-            <nav className="container-fluid navbar navbar-expand-sm bg-danger-subtle b-round-3 sticky">
+            <nav className="container-fluid navbar navbar-expand-sm bg-black sticky-top">
                 <div className="m-auto">
-                    <ul className="navbar-nav m-auto">
-                        <li className="nav-item bg-light p-1 m-2 text-info b-round-3">
+                    <ul className="navbar-nav rounded m-auto">
+                        <li className="nav-item bg-light p-1 m-2 rounded-3 text-info">
                             <a id="index" className="nav-link" href="/" data-astro-prefetch>Home</a>
                         </li>
-                        <li className="nav-item bg-light p-1 m-2 text-info">
+                        <li className="nav-item bg-light p-1 m-2 rounded-3 text-info">
                             <a className="nav-link" href="/about" data-astro-prefetch>
                                 About
                             </a>
                         </li>
-                        <li className="nav-item bg-light p-1 m-2 text-info">
+                        <li className="nav-item bg-light p-1 m-2 rounded-3 text-info">
                             <a className="nav-link" href="/contacts" data-astro-prefetch>
                                 Contact Us
                             </a>
                         </li>
-                        <li className="nav-item bg-light p-1 m-2 text-info"><a className="nav-link" href="/asite" data-astro-prefetch>About The Site</a></li>
+                        <li className="nav-item bg-light p-1 m-2 rounded-3 text-info"><a className="nav-link" href="/asite" data-astro-prefetch>About The Site</a></li>
                     </ul>
                 </div>
             </nav>
